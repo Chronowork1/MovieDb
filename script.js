578fe05af602e234f36c5cf5b9c394867b4cab5a
Jian Xie <jixie1@s.sfusd.edu>
2018-03-22 00:24:10 +0000

    march 21 done

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 /*global $*/
 $(document).ready(function(){
+    //User Input to API URL
+    
+    function inputToAPI(input) {
+        var APIUrl = "www.omdbapi.com/?apikey=90d4b10a&t=" +input+ ";
+       
+    }
+    //API URL to Object
+    function turningObjectFromAPI() {
+    
+    }
+    
+    //Show results
+    function showResults() {
+        
+    }
+    //Click handler
     
     
+    $("button").click(function(){
+        var inputText = $("input").val();
+        inputToAPI(inputText);
+    });
 });
